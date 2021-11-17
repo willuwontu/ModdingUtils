c6842cd91c947d178b7b2031cc66b56011057d76
pdcook <qphysicscook@gmail.com>
2021-11-16 21:32:31 -0500

    v0.2.4

--- ModdingUtils/ModdingUtils.cs
+++ ModdingUtils/ModdingUtils.cs
@@ -21,7 +21,7 @@ using On;
 namespace ModdingUtils
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)] // necessary for most modding stuff here
-    [BepInPlugin(ModId, ModName, "0.2.3")]
+    [BepInPlugin(ModId, ModName, "0.2.4")]
     [BepInProcess("Rounds.exe")]
     public class ModdingUtils : BaseUnityPlugin
     {
