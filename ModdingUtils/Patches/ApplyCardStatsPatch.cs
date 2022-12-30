5f1b1a7d300dc21799d46c47b4cbfa4dab7e6cb5
willuwontu <willuwontu3@gmail.com>
2022-12-29 19:59:04 -0800

    Create ApplyCardStatsPatch.cs
    Stops Readding cards from stacking objects to spawn effects in cards.

---
+++ ModdingUtils/Patches/ApplyCardStatsPatch.cs
@@ -0,0 +1,60 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ModdingUtils.Patches
+{
+    [HarmonyPatch(typeof(ApplyCardStats))]
+    class CheckBulletsAfterGettingCards
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(ApplyCardStats.CopyGunStats))]
+        private static void StopStackingObjectsToSpawn(ApplyCardStats __instance, Gun copyFromGun, Gun copyToGun)
+        {
+            List<ObjectsToSpawn> list = new List<ObjectsToSpawn>();
+
+            foreach (var objectToSpawn in copyToGun.objectsToSpawn)
+            {
+                if (copyFromGun.objectsToSpawn.Contains(objectToSpawn))
+                {
+                    ObjectsToSpawn newObj = new ObjectsToSpawn();
+                    int stacks = 0;
+                    if (objectToSpawn.stacks > 0)
+                    {
+                        stacks = objectToSpawn.stacks;
+                        objectToSpawn.stacks = 0;
+                    }
+                    CopyObjectsToSpawn(newObj, objectToSpawn);
+                    newObj.stacks = stacks;
+                    list.Add(newObj);
+                }
+                else
+                {
+                    list.Add(objectToSpawn);
+                }
+            }
+
+            copyToGun.objectsToSpawn = list.ToArray();
+        }
+
+        private static void CopyObjectsToSpawn(ObjectsToSpawn to, ObjectsToSpawn from)
+        {
+            to.effect = from.effect;
+            to.direction = from.direction;
+            to.spawnOn = from.spawnOn;
+            to.spawnAsChild = from.spawnAsChild;
+            to.numberOfSpawns = from.numberOfSpawns;
+            to.normalOffset = from.normalOffset;
+            to.stickToBigTargets = from.stickToBigTargets;
+            to.stickToAllTargets = from.stickToAllTargets;
+            to.zeroZ = from.zeroZ;
+            to.AddToProjectile = from.AddToProjectile;
+            to.removeScriptsFromProjectileObject = from.removeScriptsFromProjectileObject;
+            to.scaleStacks = from.scaleStacks;
+            to.scaleStackM = from.scaleStackM;
+            to.scaleFromDamage = from.scaleFromDamage;
+            to.stacks = from.stacks;
+        }
+    }
+}
