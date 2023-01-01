4993fcdb89c9f47c0703adbb3e72654f973dc754
willuwontu <willuwontu3@gmail.com>
2023-01-01 15:46:30 -0800

    Beep

--- ModdingUtils/ModdingUtils.cs
+++ ModdingUtils/ModdingUtils.cs
@@ -23,7 +23,7 @@ using System;
 namespace ModdingUtils
 {
     [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)] // necessary for most modding stuff here
-    [BepInPlugin(ModId, ModName, "0.4.3")]
+    [BepInPlugin(ModId, ModName, "0.4.4")]
     [BepInProcess("Rounds.exe")]
     public class ModdingUtils : BaseUnityPlugin
     {
---
+++ ModdingUtils/Utils/SortingController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace ModdingUtils.Utils
+{
+
+    public class SortingController : MonoBehaviour
+    {
+        public enum Layer
+        {
+            Default,
+            Background,
+            UI,
+            Map,
+            Card,
+            Player1,
+            Player2,
+            Player3,
+            Player4,
+            Player5,
+            Player6,
+            Player7,
+            Player8,
+            Player9,
+            Player10,
+            Front,
+            MapParticle,
+            MostFront
+        }
+
+        [SerializeField]
+        private Layer sortingLayer;
+
+        [SerializeField]
+        private int sortingOrder = 0;
+
+        [SerializeField]
+        private Renderer[] objectToSet;
+
+        private void Start()
+        {
+            foreach (var obj in objectToSet)
+            {
+                if (obj == null) continue;
+                obj.sortingLayerName = sortingLayer.ToString();
+                obj.sortingOrder = sortingOrder;
+            }
+        }
+    }
+}
